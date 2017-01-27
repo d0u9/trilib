4f6eb7fdf0c8b20bccb6dd4d86d0a527644d3ab0
d0u9 <d0u9.su@outlook.com>
2017-01-27 15:32:44 +0800

    fatal bug fix

--- core.h
+++ core.h
@@ -42,7 +42,7 @@ typedef int64_t		s64;
 
 #define BTS_PER_BYTE	8
 
-#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0])
+#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
 
 #define __round_mask(x, y) ((__typeof__(x))((y)-1))
 #define round_up(x, y) ((((x)-1) | __round_mask(x, y))+1)
